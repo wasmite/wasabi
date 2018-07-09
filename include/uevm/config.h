1ad63da7e5f052e7c643352e20abb083004c584b
Dmitry Ulanov (aka dulanov) <dulanov@gmail.com>
2018-07-09 07:19:07 +0200

    Rework project structure because some of CMake conventions

---
+++ include/uevm/config.h
@@ -0,0 +1,16 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+/**
+ * @file
+ * μEVM compilation predefines.
+ */
+
+#ifndef UEVM_CONFIG_H_
+#define UEVM_CONFIG_H_
+
+// nothing yet here
+
+#endif /* UEVM_H_ */
