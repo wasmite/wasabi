4745249c9654395017b3b7326a2aee7be08ec2dc
r8d8 <ckryvomaz@gmail.com>
2018-07-19 23:29:42 +0300

    Cleanup test utils

--- tests/uevm_test.c
+++ tests/uevm_test.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include "uevm/api.h"
 
-#define FAIL(str, line)                           \
-  do {                                            \
-    printf("Fail on line %d: '%s'\n", line, str); \
-  } while (0)
-
-#define ASSERT(expr)                    \
-  do {                                  \
-    if (!(expr)) FAIL(#expr, __LINE__); \
-  } while (0)
 
 int main() {
   ASSERT(uevm_add(0, 0) == 0);
