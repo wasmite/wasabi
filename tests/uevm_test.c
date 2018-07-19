6346401d0363d91b7180b769b2675b5c4df14631
Dmitry Ulanov (aka dulanov) <dulanov@gmail.com>
2018-07-19 18:24:16 +0200

    Add initial functional tests

--- tests/uevm_test.c
+++ tests/uevm_test.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include "uevm/api.h"
 
+#define FAIL(str, line)                           \
+  do {                                            \
+    printf("Fail on line %d: '%s'\n", line, str); \
+  } while (0)
+
+#define ASSERT(expr)                    \
+  do {                                  \
+    if (!(expr)) FAIL(#expr, __LINE__); \
+  } while (0)
+
 int main() {
-    printf("Tests completed\n");
-    return 0;
+  ASSERT(uevm_add(0, 0) == 0);
+  ASSERT(uevm_add(0, 1) == 1);
+  ASSERT(uevm_add(1, 0) == 1);
+  ASSERT(uevm_add(1, 1) == 2);
+  printf("Tests completed\n");
+  return 0;
 }
